3eaf5de6cac48b03d626661f984691245c365a9c
Leonardo Kegel <leonardo.kegel@gmail.com>
2025-11-06 20:32:43 -0300

    initial commit

---
+++ src/main/java/br/com/fiap/baitersburger/customer/BaitersBurgerCustomerApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.baitersburger.customer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaitersBurgerCustomerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BaitersBurgerCustomerApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/fiap/baitersburger/customer/BaitersBurgerCustomerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.baitersburger.customer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaitersBurgerCustomerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
